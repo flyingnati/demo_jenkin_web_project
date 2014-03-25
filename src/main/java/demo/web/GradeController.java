35601387dd980861000639ee1656b76b54eac2da
Somkiat <somkiat.p@gmail.com>
2014-03-25 15:58:11 +0700

    Add web project for demo in Jenkins course

---
+++ src/main/java/demo/web/GradeController.java
@@ -0,0 +1,40 @@
+package demo.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import demo.jenkins.Grade;
+
+@SuppressWarnings("serial")
+@WebServlet(name = "GradeController", urlPatterns = { "/grade/convert" })
+public class GradeController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+	}
+
+	private void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		ServletOutputStream out = resp.getOutputStream();
+		String result = "Unknown";
+		String score = req.getParameter("score");
+		if( score != null && !score.equals("") ) {
+			Grade grade = new Grade();
+			result = grade.convert(Integer.parseInt(score));
+		}
+		out.println(result);
+		out.flush();
+		out.close();
+	}
+}
